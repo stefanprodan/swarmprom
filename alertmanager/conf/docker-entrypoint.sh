60964cf41d0830cfbc08153db23e72777cb2293d
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-10 11:28:55 +0300

    add # to Slack channel

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 cat /etc/alertmanager/alertmanager.yml |\
     sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
-    sed "s@#channel: <channel>#@channel: '$SLACK_CHANNEL'@g" > /tmp/alertmanager.yml
+    sed "s@#channel: <channel>#@channel: '#$SLACK_CHANNEL'@g" > /tmp/alertmanager.yml
 
 mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
 
