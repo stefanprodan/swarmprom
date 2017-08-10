04830e1ba7c495faef857b46d95c3cd978737c88
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-10 10:18:10 +0300

    log slack settings

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
-cat /etc/alertmanager/alertmanager.yml | \
-    sed "s@#channel: <channel>#@channel: '$SLACK_CHANNEL'@g" |\
-    sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" > /tmp/alertmanager.yml
+echo "Setting up Slack $SLACK_URL' '$SLACK_CHANNEL'"
+
+cat /etc/alertmanager/alertmanager.yml |\
+    sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
+    sed "s@#channel: <channel>#@channel: '$SLACK_CHANNEL'@g" > /tmp/alertmanager.yml
 
 mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
 
