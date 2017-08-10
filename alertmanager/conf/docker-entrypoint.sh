df002c75d68a040c4050a29b000216febb38139f
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-10 15:36:19 +0300

    fix slack username

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 cat /etc/alertmanager/alertmanager.yml |\
     sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
     sed "s@#channel: <channel>#@channel: '#$SLACK_CHANNEL'@g" |\
-    sed "s@#username: <user>#@username: '#$SLACK_USER'@g" > /tmp/alertmanager.yml
+    sed "s@#username: <user>#@username: '$SLACK_USER'@g" > /tmp/alertmanager.yml
 
 mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
 
