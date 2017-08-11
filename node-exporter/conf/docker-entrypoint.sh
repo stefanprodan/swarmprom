9225811ddf7d2e921f48c06da354677f0e14c208
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-11 15:30:25 +0300

    rename node meta

--- node-exporter/conf/docker-entrypoint.sh
+++ node-exporter/conf/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 NODE_NAME=$(cat /etc/nodename)
-echo "node_id{node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
+echo "node_meta{node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
 
 set -- /bin/node_exporter "$@"
 
