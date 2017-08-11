ac19a35255d61e0f8273ae79f3b93d6a6bb471ad
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-11 09:59:35 +0300

    mount and collect hostname

--- node-exporter/conf/docker-entrypoint.sh
+++ node-exporter/conf/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
-echo "node_id{node_id=\"$NODE_ID\"} 1" > /etc/node-exporter/nodeid.prom
+NODE_NAME=$(cat /etc/nodename)
+echo "node_id{node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
 
 set -- /bin/node_exporter "$@"
 
