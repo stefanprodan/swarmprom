53bde823814ea63abcdf4d734c3c3dd9523a5923
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-12 17:43:14 +0300

    node meta label matching cAdvisor node id

--- node-exporter/conf/docker-entrypoint.sh
+++ node-exporter/conf/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 NODE_NAME=$(cat /etc/nodename)
-echo "node_meta{node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
+echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
 
 set -- /bin/node_exporter "$@"
 
