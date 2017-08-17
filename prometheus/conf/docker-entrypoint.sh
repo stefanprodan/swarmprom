f58ed6dfd41e16eebb1dc0fd285ef95a030e0507
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-17 10:36:03 +0300

    undo bash

--- prometheus/conf/docker-entrypoint.sh
+++ prometheus/conf/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
 cat /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
 cat /etc/prometheus/weave-cortex.yml | \
@@ -6,37 +6,37 @@ cat /etc/prometheus/weave-cortex.yml | \
 
 # JOBS=mongo-exporter:9111,redis-exporter:9112
 
-if [ ${JOBS+x} ]; then
-IFS=,
-ary=($JOBS)
-for key in "${!ary[@]}"; do
-echo "adding job ${ary[$key]}"
-
-SERVICE="$(cut -d':' -f1 <<<"${ary[$key]}")"
-PORT="$(cut -d':' -f2 <<<"${ary[$key]}")"
-
-cat >>/tmp/prometheus.yml <<EOF
-  - job_name: '${SERVICE}'
-    dns_sd_configs:
-    - names:
-      - 'tasks.${SERVICE}'
-      type: 'A'
-      port: ${PORT}
-
-EOF
-
-cat >>/tmp/weave-cortex.yml <<EOF
-  - job_name: '${SERVICE}'
-    dns_sd_configs:
-    - names:
-      - 'tasks.${SERVICE}'
-      type: 'A'
-      port: ${PORT}
-
-EOF
-
-done
-fi
+#if [ ${JOBS+x} ]; then
+#IFS=,
+#ary=($JOBS)
+#for key in "${!ary[@]}"; do
+#echo "adding job ${ary[$key]}"
+#
+#SERVICE="$(cut -d':' -f1 <<<"${ary[$key]}")"
+#PORT="$(cut -d':' -f2 <<<"${ary[$key]}")"
+#
+#cat >>/tmp/prometheus.yml <<EOF
+#  - job_name: '${SERVICE}'
+#    dns_sd_configs:
+#    - names:
+#      - 'tasks.${SERVICE}'
+#      type: 'A'
+#      port: ${PORT}
+#
+#EOF
+#
+#cat >>/tmp/weave-cortex.yml <<EOF
+#  - job_name: '${SERVICE}'
+#    dns_sd_configs:
+#    - names:
+#      - 'tasks.${SERVICE}'
+#      type: 'A'
+#      port: ${PORT}
+#
+#EOF
+#
+#done
+#fi
 
 mv /tmp/prometheus.yml /etc/prometheus/prometheus.yml
 mv /tmp/weave-cortex.yml /etc/prometheus/weave-cortex.yml
