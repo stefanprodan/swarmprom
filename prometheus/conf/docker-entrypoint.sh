4031e2570c40735e55ccedc1151a8a442eaefb64
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-17 11:31:18 +0300

    cleanup

--- prometheus/conf/docker-entrypoint.sh
+++ prometheus/conf/docker-entrypoint.sh
@@ -4,39 +4,7 @@ cat /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
 cat /etc/prometheus/weave-cortex.yml | \
     sed "s@#password: <token>#@password: '$WEAVE_TOKEN'@g" > /tmp/weave-cortex.yml
 
-#JOBS="mongo-exporter:9111 redis-exporter:9112"
-
-#if [ ${JOBS+x} ]; then
-#IFS=,
-#ary=($JOBS)
-#for key in "${!ary[@]}"; do
-#echo "adding job ${ary[$key]}"
-#
-#SERVICE="$(cut -d':' -f1 <<<"${ary[$key]}")"
-#PORT="$(cut -d':' -f2 <<<"${ary[$key]}")"
-#
-#cat >>/tmp/prometheus.yml <<EOF
-#  - job_name: '${SERVICE}'
-#    dns_sd_configs:
-#    - names:
-#      - 'tasks.${SERVICE}'
-#      type: 'A'
-#      port: ${PORT}
-#
-#EOF
-#
-#cat >>/tmp/weave-cortex.yml <<EOF
-#  - job_name: '${SERVICE}'
-#    dns_sd_configs:
-#    - names:
-#      - 'tasks.${SERVICE}'
-#      type: 'A'
-#      port: ${PORT}
-#
-#EOF
-#
-#done
-#fi
+#JOBS=mongo-exporter:9111 redis-exporter:9112
 
 if [ ${JOBS+x} ]; then
 
