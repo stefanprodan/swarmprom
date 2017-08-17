26bc1161a8dd6235995da909868f4c96e7447f5f
Stefan Prodan <stefan.prodan@gmail.com>
2017-08-17 11:18:25 +0300

    try fix cannot unmarshal port into int

--- prometheus/conf/docker-entrypoint.sh
+++ prometheus/conf/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
-cat /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
-cat /etc/prometheus/weave-cortex.yml | \
-    sed "s@#password: <token>#@password: '$WEAVE_TOKEN'@g" > /tmp/weave-cortex.yml
+#cat /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
+#cat /etc/prometheus/weave-cortex.yml | \
+#    sed "s@#password: <token>#@password: '$WEAVE_TOKEN'@g" > /tmp/weave-cortex.yml
 
-# JOBS="mongo-exporter:9111 redis-exporter:9112"
+JOBS="mongo-exporter:9111 redis-exporter:9112"
 
 #if [ ${JOBS+x} ]; then
 #IFS=,
@@ -47,34 +47,34 @@ echo "adding job $job"
 SERVICE=$(echo "$job" | cut -d":" -f1)
 PORT=$(echo "$job" | cut -d":" -f2)
 
-cat >>/tmp/prometheus.yml <<EOF
+cat >>prometheus.yml <<EOF
+
   - job_name: '${SERVICE}'
     dns_sd_configs:
     - names:
       - 'tasks.${SERVICE}'
       type: 'A'
       port: ${PORT}
-
 EOF
 
-cat >>/tmp/weave-cortex.yml <<EOF
+cat >>weave-cortex.yml <<EOF
+
   - job_name: '${SERVICE}'
     dns_sd_configs:
     - names:
       - 'tasks.${SERVICE}'
       type: 'A'
       port: ${PORT}
-
 EOF
 
 done
 
 fi
 
-mv /tmp/prometheus.yml /etc/prometheus/prometheus.yml
-mv /tmp/weave-cortex.yml /etc/prometheus/weave-cortex.yml
-
-set -- /bin/prometheus "$@"
-
-exec "$@"
+#mv /tmp/prometheus.yml /etc/prometheus/prometheus.yml
+#mv /tmp/weave-cortex.yml /etc/prometheus/weave-cortex.yml
+#
+#set -- /bin/prometheus "$@"
+#
+#exec "$@"
 
