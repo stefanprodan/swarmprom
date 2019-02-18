439e5857939f01f7c87e92f62449489e0095203b
Alessandro Staffolani <alestam93@gmail.com>
2019-02-18 11:21:08 +0100

    Changed alertmanager's config

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 
-cat /etc/alertmanager/alertmanager.yml |\
-    sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
-    sed "s@#channel: <channel>#@channel: '#$SLACK_CHANNEL'@g" |\
-    sed "s@#username: <user>#@username: '$SLACK_USER'@g" > /tmp/alertmanager.yml
+# cat /etc/alertmanager/alertmanager.yml |\
+#    sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
+#    sed "s@#channel: <channel>#@channel: '#$SLACK_CHANNEL'@g" |\
+#    sed "s@#username: <user>#@username: '$SLACK_USER'@g" > /tmp/alertmanager.yml
 
 mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
 
-set -- /bin/alertmanager "$@"
+# set -- /bin/alertmanager "$@"
 
-exec "$@"
+# exec "$@"
